4ba9d5a605549e661219e305e8aea20eb97ec507
Minh Nguyen <minhna@gmail.com>
2022-12-24 23:30:53 +0700

    update sample

--- samples/react-component.js
+++ samples/react-component.js
@@ -20,35 +20,35 @@ const FirstComponent = ({ a, b, as1, as2, ...rest }) => {
   );
 };
 
-// export const SecondComponent = () => {
-//   const async2 = async () => {
-//     const props = { otherProps: "Not valid" };
-//   };
+export const SecondComponent = () => {
+  const async2 = async () => {
+    const props = { otherProps: "Not valid" };
+  };
 
-//   const props = {
-//     c: "C",
-//     d: ["d1", "d2"],
-//     s1: () => {},
-//     s2: async function () {},
-//   };
+  const props = {
+    c: "C",
+    d: ["d1", "d2"],
+    s1: () => {},
+    s2: async function () {},
+  };
 
-//   return (
-//     <div>
-//       <FirstComponent
-//         a='A'
-//         b='B'
-//         as1={async1}
-//         as2={async2}
-//         as3={async () => {}}
-//         f4={function () {}}
-//         {...props}
-//       />
-//       <FirstExported as1={async1} as2={async2} {...props} />
-//       <SecondExported as1={async1} />
-//       <ThirdExported as2={async2} />
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <FirstComponent
+        a='A'
+        b='B'
+        as1={async1}
+        as2={async2}
+        as3={async () => {}}
+        f4={function () {}}
+        {...props}
+      />
+      <FirstExported as1={async1} as2={async2} {...props} />
+      <SecondExported as1={async1} />
+      <ThirdExported as2={async2} />
+    </div>
+  );
+};
 
 export default withTracker(() => {
   const s = {
